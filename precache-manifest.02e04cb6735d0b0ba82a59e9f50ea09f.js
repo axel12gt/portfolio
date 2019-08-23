a99d90584f3c6ea771380c25ef6b8ea8a3d8fcb4
Dameon Brown <axel12gt@live.com>
2019-08-22 22:51:51 -0400

    Updates

---
+++ precache-manifest.02e04cb6735d0b0ba82a59e9f50ea09f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2efb14e27a79683ad6ae9d0d2de4fc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c7d0cd90997da44c447",
+    "url": "/portfolio/static/css/main.8aa276d5.chunk.css"
+  },
+  {
+    "revision": "8c61dcbe158dca2aecfe",
+    "url": "/portfolio/static/js/2.8ffd91fb.chunk.js"
+  },
+  {
+    "revision": "3c7d0cd90997da44c447",
+    "url": "/portfolio/static/js/main.ca529141.chunk.js"
+  },
+  {
+    "revision": "e8aab94e553ef1acd8d3",
+    "url": "/portfolio/static/js/runtime~main.e9a507a1.js"
+  },
+  {
+    "revision": "64b15ce2bb964ffc22a023d59defb2aa",
+    "url": "/portfolio/static/media/Crystal-Collector.64b15ce2.png"
+  },
+  {
+    "revision": "9ed4d12cd4efc6305a7c7f4f64c73945",
+    "url": "/portfolio/static/media/b.9ed4d12c.png"
+  },
+  {
+    "revision": "33589ddda51a4dc8d56c6167b9e0e544",
+    "url": "/portfolio/static/media/board.33589ddd.png"
+  },
+  {
+    "revision": "86ced3633fa51ce2059f1e18ef804573",
+    "url": "/portfolio/static/media/giphy.86ced363.gif"
+  }
+]);
